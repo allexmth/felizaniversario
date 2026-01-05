aa7ad55e2c38a31e895b97f4679b8d8cfa2bf7cb
Allex Santos <allexmatiasx@gmail.com>
2026-01-05 10:10:04 -0300

    Update birthday message in script.js
    CORREÇÕES DE BUGS

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'FELIZ', 'ANIVERSARIO', ' TE AMO MUITO	!' ],
+			strings: [ 'FELIZ', 'ANIVERSARIO', ' GOSTO MUITO DE VOCE!' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
